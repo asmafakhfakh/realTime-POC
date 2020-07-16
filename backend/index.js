94cf3950ceac0256709946d96a74b828da9be9d6
Asma <asma.fakhfakh@futura.tn>
2020-07-16 11:45:11 +0100

    blaaaaaa

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,39 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
-const port = 3000;
+const port = 3010;
 
-
-let messages =[]
-
+let messages = []
 
 io.on("connection", socket => {
+
+    console.log("user connected", socket.id);
+    // //Get the chatID of the user and join in a room of the same chatID
+    // chatID = socket.handshake.query.chatID
+    // socket.join(chatID)
+    // //Leave the room if the user closes the socket
+    // socket.on('disconnect', () => {
+    //     socket.leave(chatID)
+    //     console.log("Disconnected")
+    // })
+
     socket.on("chat message", msg => {
-      io.emit("chat message", msg);
-      messages = [...messages, msg]
-      console.log('msgs backend ',messages )
-      
+
+        // receiverChatID = msg.receiverChatID
+        //   senderChatID = msg.senderChatID
+        //   content = msg.content
+        // //Send message to only that particular room
+        // socket.in(receiverChatID).emit('receive_message', {
+        //     'content': content,
+        //     'senderChatID': senderChatID,
+        //     'receiverChatID':receiverChatID,
+        // })
+
+        io.emit("chat message", msg);
+        messages = [...messages, msg]
+        console.log('msgs backend ', messages)
+
     });
-  });
+});
 
-  server.listen(port, () => console.log("server running on port:" + port));
-
+server.listen(port, () => console.log("server running on port:" + port));
